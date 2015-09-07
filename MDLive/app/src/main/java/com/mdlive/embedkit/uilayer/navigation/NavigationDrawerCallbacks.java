e8dacb5bb569b3c422a1a92de3a178121871a56b
Raja Rathinavel <raja.rathinavel@photoninfotech.net>
2015-09-07 13:56:26 +0530

    Removal of duplicate app module from dev.

--- MDLive/app/src/main/java/com/mdlive/embedkit/uilayer/navigation/NavigationDrawerCallbacks.java
+++
@@ -1,8 +0,0 @@
-package com.mdlive.embedkit.uilayer.navigation;
-
-/**
- * Created by sambathkumar_ka on 04-Jun-2015.
- */
-public interface NavigationDrawerCallbacks {
-    void onNavigationDrawerItemSelected(int position);
-}
